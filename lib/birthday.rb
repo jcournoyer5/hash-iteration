427b94a26acb6d639a6f146ed9697841d3aea955
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-20 15:53:02 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,18 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |person, age|
+      puts "Happy Birthday #{person}! You are now #{age} years old!"
+  end
 end
 
-
+def select_winner(passengers)
+    winner = nil
+        passengers.each do |room, person|
+            if room== :suite_a && person.start_with?("A")
+                winner = person
+            end
+        end
+    winner
+end    
+    
